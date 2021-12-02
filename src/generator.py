b9edb50f2a62e9a1c13a41141f4da2b9cd2bcd4f
Florian Jüngermann <florianjuengermann@gmail.com>
2021-12-02 16:36:59 -0500

    added seed to generator

--- src/generator.py
+++ src/generator.py
@@ -1,25 +1,26 @@
 import numpy as np
 
 
-def getRatingAndPropensityMatrix(inv_scale=1, alpha=0.01, r=5):
+def getRatingAndPropensityMatrix(inv_scale=1, alpha=0.01, r=5, seed=0):
+    rng = np.random.default_rng(seed)
     num_users = int(80/inv_scale)  # m
     num_movies = int(80/inv_scale)  # n
     latent_dim = r
     num_core_users = int(20/inv_scale)  # m_core
     num_core_movies = int(20/inv_scale)  # n_core
     # users
-    core_user_matrix = np.random.normal(size=(num_core_users, latent_dim))
+    core_user_matrix = rng.normal(size=(num_core_users, latent_dim))
     dirichlet_alphas = [alpha]*num_core_users
-    B_transform_matrix = np.random.dirichlet(
+    B_transform_matrix = rng.dirichlet(
         dirichlet_alphas, size=num_users-num_core_users)
     # is a linear combination of core users
     non_core_user_matrix = B_transform_matrix @ core_user_matrix
     latent_user_matrix = np.vstack((core_user_matrix, non_core_user_matrix))
 
     # movies
-    core_movie_matrix = np.random.normal(size=(num_core_movies, latent_dim))
+    core_movie_matrix = rng.normal(size=(num_core_movies, latent_dim))
     dirichlet_alphas = [0.01]*num_core_movies
-    B_transform_matrix = np.random.dirichlet(
+    B_transform_matrix = rng.dirichlet(
         dirichlet_alphas, size=num_movies-num_core_movies)
     # is a linear combination of core movies
     non_core_movie_matrix = B_transform_matrix @ core_movie_matrix
@@ -68,21 +69,22 @@ def getRatingAndPropensityMatrix(inv_scale=1, alpha=0.01, r=5):
     return rating_matrix, P
 
 
-def getRatingAndPropensityMatrix_general(latent_movie_matrix=None, inv_scale=1, alpha=0.01, r=5):
+def getRatingAndPropensityMatrix_general(latent_movie_matrix=None, inv_scale=1, alpha=0.01, r=5, seed=0):
+    rng = np.random.default_rng(seed)
     num_users = int(80/inv_scale)  # m
     num_movies = int(80/inv_scale)  # n
     latent_dim = r
     num_core_movies = int(30/inv_scale)  # n_core
 
     # users
-    latent_user_matrix = np.random.normal(size=(num_users, latent_dim))
+    latent_user_matrix = rng.normal(size=(num_users, latent_dim))
 
     # movies
     if latent_movie_matrix is None:
-        core_movie_matrix = np.random.normal(
+        core_movie_matrix = rng.normal(
             size=(num_core_movies, latent_dim))
         dirichlet_alphas = [alpha]*num_core_movies
-        B_transform_matrix = np.random.dirichlet(
+        B_transform_matrix = rng.dirichlet(
             dirichlet_alphas, size=num_movies-num_core_movies)
         # is a linear combination of core movies
         non_core_movie_matrix = B_transform_matrix @ core_movie_matrix
